5afb0d535e103ced1d309a531eb7298bac8c8390
girishrma57 <girishrma57>
2023-09-06 19:56:23 +0000

    new

---
+++ curltest.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %sh
+# MAGIC curl https://repos.spark-packages.org/com/databricks/spark-xml_2.12/0.16.0/spark-xml_2.12-0.16.0.jar
+
+# COMMAND ----------
+
+
---
+++ git-submodule.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %sh git submodule sync --recursive https://github.com/girishrma57/testpub.git
+
+# COMMAND ----------
+
+
