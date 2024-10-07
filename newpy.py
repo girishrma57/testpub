3edaafb42fb9c88d60f0e7e4aef811a5e1f446d3
Girish Sharma <116057064+girishrma57@users.noreply.github.com>
2024-10-08 00:02:33 +0530

    Create newpy.py

---
+++ newpy.py
@@ -0,0 +1,12 @@
+import os
+
+# Assuming you cloned the repository to a directory called 'repository'
+repo_dir = '/Workspace/Repos/girish.sharma@databricks.com/girishrepo'
+sql_file_path = os.path.join(repo_dir, '/Workspace/Repos/girish.sharma@databricks.com/girishrepo/mytestsql.sql')
+
+# Read the SQL file
+with open(sql_file_path, 'r') as file:
+    sql_query = file.read()
+
+# Now you can use sql_query to execute it in your database
+print(sql_query)
