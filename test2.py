e90db37844c1667c03a3b0c69b088b59993f1640
Girish Sharma <116057064+girishrma57@users.noreply.github.com>
2024-10-07 23:56:07 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,4 +1,12 @@
-# Databricks notebook source
-1+1
-2*2
-100*100
+import os
+
+# Assuming you cloned the repository to a directory called 'repository'
+repo_dir = '/Workspace/Repos/girish.sharma@databricks.com/testpub'
+sql_file_path = os.path.join(repo_dir, '/Workspace/Repos/girish.sharma@databricks.com/testpub/mytestsql.sql')
+
+# Read the SQL file
+with open(sql_file_path, 'r') as file:
+    sql_query = file.read()
+
+# Now you can use sql_query to execute it in your database
+print(sql_query)
